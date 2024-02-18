5f686830b654c1701326eb098c7be56cf98959e6
Rodrigo Rivas <rodroy019@hotmail.com>
2024-02-18 02:15:17 -0300

    add: se agrega la funcion de convetir valor al elegido

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,42 @@
 const btnConvertir = document.getElementById('btnConvertir');
 const inputCLP = document.getElementById('inputCLP');
 const monedaSeleccionada = document.getElementById('monedaSeleccionada');
+const spanTotal = document.getElementById('spanTotal');
 const apiUrl = 'https://mindicador.cl/api/';
+const fechaActual = new Date();
 
 agregarOpcionesDesdeAPI();
 
 
 
-let convertir  = () => {
-
+async function convertir(){
+    if(inputCLP.value != ''){
+        const valorMoneda = await buscarMoneda(monedaSeleccionada.value);
+        let valorCovertido = (inputCLP.value / valorMoneda);
+        spanTotal.innerHTML = valorCovertido;
+    }else{
+        alert("Debes ingresar un monto a convertir");
+        inputCLP.value = "";
+    }
 }
 
 btnConvertir.addEventListener('click', convertir)
 
 
+async function buscarMoneda(moneda){
+    try{
+        const resConvertir = await fetch(`${apiUrl}${moneda}`);
+        const dataConvertir = await resConvertir.json();
+        const { serie } = dataConvertir;
+        const [{valor: valorMoneda}] = serie;
+        console.log(valorMoneda);
+        return valorMoneda;
+    
+    }catch (error){
+        console.log(error);
+    }
 
+}
 
 
 
