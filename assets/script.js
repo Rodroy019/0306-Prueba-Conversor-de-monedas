b7dafd97382eae802fc712cbc8e06f2f5936876f
Rodrigo Rivas <rodroy019@hotmail.com>
2024-02-18 00:33:09 -0300

    add: se agraga funcion que agrega los tipos de monedas a convertir en los options desde Api

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,42 @@
 const btnConvertir = document.getElementById('btnConvertir');
-const monedaSeleccionada = document.getElementById('monedaSeleccionada')
+const inputCLP = document.getElementById('inputCLP');
+const monedaSeleccionada = document.getElementById('monedaSeleccionada');
 const apiUrl = 'https://mindicador.cl/api/';
-let tablaHTML = '';
+
+agregarOpcionesDesdeAPI();
+
+
 
 let convertir  = () => {
 
 }
+
 btnConvertir.addEventListener('click', convertir)
 
 
-fetch(apiUrl)
-  .then(res => res.json())
-  console.log(res)
-  .then(data => {
-    data.forEach(item => {
-      const option = document.createElement('option');
-      option.value = item.codigo;
-      option.textContent = item.nombre;
-      monedaSeleccionada.appendChild(option);
-    });
-  })
-  .catch(error => {
-    console.error('Hubo un problema al obtener los datos:', error);
-  });
 
 
-async function getData(){
-    try{
-        const res = await fetch("https://mindicador.cl/api/");
-        const datas = await res.json();
-        console.log(datas);
-    }catch (error){
-        alert(error.message);
+
+
+
+
+
+async function agregarOpcionesDesdeAPI() {
+    try {
+
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+
+        for (const [codigo, info] of Object.entries(data)) {
+            if (codigo && info.nombre) {
+                const option = document.createElement('option');
+                option.value = codigo;
+                option.textContent = `${info.nombre}`;
+                monedaSeleccionada.appendChild(option);
+            }
+        }
+    } catch (error) {
+        console.error('Error al obtener datos de la API:', error);
     }
-    }
+}
+
